952b133b2fffee101fd642019336c62ef2e99549
Jessica Borch <jessica.borch@tietoevry.com>
2023-01-01 17:30:33 +0100

    updated content

--- lost-in-translation/gender-bias.py
+++ lost-in-translation/gender-bias.py
@@ -33,12 +33,11 @@ do_adj = False
 do_two_adj = False
 do_occ = False
 do_adj_occ = False
-do_two_adj_occ = True
 do_both = False
 do_translation = False
 
 if do_adj:
-	adjectives = list(csv.reader(open('adjectives/adj-hoyle.tsv','r'), delimiter='\t'))
+	adjectives = list(csv.reader(open('adjectives/adjectives.tsv','r'), delimiter='\t'))
 	with open('Results/result-adj.tsv','w') as output:
 		output.write("Adjective English")
 		output.write("\tAdjective Swedish")
@@ -230,84 +229,3 @@ if do_adj_occ:
 				output.write('\n')
 				output.flush()
 
-if do_two_adj_occ:
-	adjectives_result = list(csv.reader(open('adjectives/selected-adjectives.tsv','r'), delimiter='\t'))
-	with open('Results/result-two-adj-occ.tsv','w') as output:
-		output.write("First Adjective English")
-		output.write("\tFirst Adjective Swedish")
-		output.write("\tFirst Adjective Gender Category")
-		output.write("\tFirst Adjective Quality")
-		output.write("\tFirst Adjective Translated Pronoun")
-		output.write("\tSecond Adjective English")
-		output.write("\tSecond Adjective Swedish")
-		output.write("\tSecond Adjective Gender Category")
-		output.write("\tSecond Adjective Quality")
-		output.write("\tSecond Adjective Translated Pronoun")
-		output.write("\tOccupation English")
-		output.write("\tOccupation Swedish")
-		output.write("\tOccupation Gender Category")
-		output.write("\tOccupation US Gender Category")
-		output.write("\tOccupation Sweden Gender Category")
-		output.write("\tOccupation Translated Pronoun")
-		output.write("\tCombination Category")
-		output.write("\tTwo Adjectives and Occupation Translated Sentence")
-		output.write("\tTwo Adjectives and Occupation Pronoun")
-		output.write('\n')
-
-		for entry in adjectives_result[1:]:
-
-			adj_english 		= entry[0]
-			adj_swedish			= entry[1]
-			adj_gender 			= entry[2]
-			adj_quality 		= entry[3]
-			adj_pronoun			= entry[5]
-   
-   
-			for entry in adjectives_result[1:]:
-
-				sec_adj_english			= entry[0]
-				sec_adj_swedish			= entry[1]
-				if (sec_adj_swedish != adj_swedish):
-					sec_adj_gender 			= entry[2]
-					sec_adj_quality 		= entry[3]
-					sec_adj_pronoun			= entry[5]
-   
-					occupation_result = list(csv.reader(open('Results/result-occ.tsv','r'), delimiter='\t'))
-					for entry in occupation_result[1:]:
-    
-						occ_english			= entry[0]
-						occ_swedish 		= entry[1]
-						occ_gender			= entry[2]
-						occ_us_gender 		= entry[3]
-						occ_swe_gender		= entry[4]
-						occ_pronoun			= entry[6]
-            
-						output.write(adj_english)
-						output.write('\t' + adj_swedish)
-						output.write('\t' + adj_gender)
-						output.write('\t' + adj_quality)	
-						output.write('\t' + adj_pronoun)
-						output.write('\t' + sec_adj_english)
-						output.write('\t' + sec_adj_swedish)
-						output.write('\t' + sec_adj_gender)
-						output.write('\t' + sec_adj_quality)
-						output.write('\t' + sec_adj_pronoun)
-						output.write('\t' + occ_english)
-						output.write('\t' + occ_swedish)
-						output.write('\t' + occ_gender)
-						output.write('\t' + occ_us_gender)
-						output.write('\t' + occ_swe_gender)
-						output.write('\t' + occ_pronoun)
-   
-						phrase = "hen är en {} och {} {}".format(adj_swedish, sec_adj_swedish, occ_swedish)
- 
-						translated_sentence = translate_text('en', phrase).lower()
-						print("Phrase: {} | Translation: {}".format(phrase, translated_sentence))
-
-						combination_category = adj_pronoun[0] + sec_adj_pronoun[0] + occ_pronoun[0]
-						output.write('\t' + combination_category)
-						output.write('\t' + translated_sentence)			
-						translated_pronoun = get_pronoun(translated_sentence)
-						output.write('\t' + translated_pronoun)
-						output.write('\n')
-			output.flush()
